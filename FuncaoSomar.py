9e13f9347123850cb0af0f8a837ca6fc4d9f204a
Cristhian Santos <70783719+CristhianFSantos@users.noreply.github.com>
2020-11-20 21:46:02 -0300

    Tarefa_01

---
+++ FuncaoSomar.py
@@ -0,0 +1,9 @@
+def somarNumeros(numeroUm, numeroDois):
+    n1 = int(numeroUm)
+    n2 = int(numeroDois)
+    soma = n1 + n2
+    return soma
+
+
+somaGeral = somarNumeros(10, 5)
+print(f'\nA soma dos números 10 e 5 é: {somaGeral}')
